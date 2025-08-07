87b9e5a15426ecd755d1799e4479a80291f48755
Iveta Círová <cirova.iveta@ptw.cz>
2025-08-07 14:57:22 +0200

    recipeCard hover

--- src/components/RecipeCard/index.jsx
+++ src/components/RecipeCard/index.jsx
@@ -3,9 +3,11 @@ import "./style.css"
 
 export const RecipeCard = ({photo_url, title, openDetail}) => {
   return <div className="recipe">
-    <div className="recipe_img" onClick={openDetail}>
+    <div className="recipe__img" onClick={openDetail}>
         <img src={`./imgRecipe/${photo_url}`} alt={title} className="img" />
-        <h1 className="recipe__title">{title}</h1>
+       
     </div>
+
+     <h1 className="recipe__title">{title}</h1>
   </div>
 }
